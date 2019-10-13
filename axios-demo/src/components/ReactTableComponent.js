ebc7d141c72bdd092eabe950fa14d2587f478c4b
kavitagupta972 <kavitagupta972@gmail.com>
2019-10-13 12:31:26 +0530

    Added Modal Pop Up and Bootstrap

--- axios-demo/src/components/ReactTableComponent.js
+++ axios-demo/src/components/ReactTableComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Modal, ModalFooter, ModalHeader, ModalBody } from 'reactstrap';
 
 // Import React Table
 import ReactTable from "react-table";
@@ -7,26 +8,47 @@ import ApiService from "../service/ApiService";
 import './ReactTableComponent.css';
 
 class ReactTableComponent extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       data: [],
+      modal: false
     };
+
+
     this.columns = [
       {
         Header: "Action",
         accessor: "action",
         headerStyle: { whitespace: 'unset' },
         style: { whitespace: 'unset' },
-        Cell: ({ row }) => {
+        Cell: ({ row, original }) => {
 
           return (
             <div>
+
+              {/* Modal Example */}
+              <div>
+
+                <Button color="danger" onClick={this.toggle}>TOGGLE</Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle}
+                  fade={this.state.fade}
+                >
+                  <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                  <ModalBody>
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+              {/* Modal Example */}
               <span
                 className="basicLink"
                 style={{ cursor: 'pointer', marginRight: '10px' }}
                 onClick={() => {
-                  this.editRow(row);
+                  this.editRow(original);
                 }}
               >
                 {'Edit'}
@@ -35,7 +57,7 @@ class ReactTableComponent extends React.Component {
                 className="basicLink"
                 style={{ cursor: 'pointer', marginRight: '10px' }}
                 onClick={() => {
-                  this.deleteRow(row);
+                  this.deleteRow(original);
                 }}
               >
                 {'Delete'}
@@ -70,12 +92,20 @@ class ReactTableComponent extends React.Component {
       },
     ];
   }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
   editRow = row => {
+    // TODO: Edit Call here
     console.log("inside edit row", row);
     ApiService.editEmployee({
       "first_name": "kavita",
       "last_name": "gupta",
-      "email": "kavitagupta972@gmail.com",
+      "email": "Hohndoe@gmail.com",
       "id": row.id
     }).then(res => {
       console.log("in edit method ...");
@@ -98,6 +128,8 @@ class ReactTableComponent extends React.Component {
 
   deleteRow = row => {
     console.log("inside delete row ", row.id);
+
+    // TODO: Delete Confirm Box
     ApiService.deleteEmployee(row.id).then(res => {
       console.log("in delete method .....");
       this.reloadData();
@@ -123,17 +155,13 @@ class ReactTableComponent extends React.Component {
     return (
       <div style={{ padding: '50px' }}>
         <button className="btn btn-danger" style={{ width: '100px', marginLeft: '1200px' }} onClick={() => this.addRow()}>Add Record</button>
-        <br />
-        <br />
         <ReactTable
           data={data}
           defaultPageSize={10}
           columns={this.columns}
           className="-striped -highlight"
         />
-        <br />
-
-      </div >
+      </div>
     );
   }
 }
